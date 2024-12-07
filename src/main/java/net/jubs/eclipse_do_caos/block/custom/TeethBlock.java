4b83d1f349f271fd170ececcba116f1762ee0e2a
Ju <gabrielgcm.martins@gmail.com>
2024-12-07 15:48:30 -0300

    Fixed Teeth Block

--- src/main/java/net/jubs/eclipse_do_caos/block/custom/TeethBlock.java
+++ src/main/java/net/jubs/eclipse_do_caos/block/custom/TeethBlock.java
@@ -1,9 +1,39 @@
 package net.jubs.eclipse_do_caos.block.custom;
 
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.core.Direction;
 
 public class TeethBlock extends Block {
-    public TeethBlock(Properties pProperties) {
-        super(pProperties);
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
+
+    public TeethBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, net.minecraft.world.level.block.Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, net.minecraft.world.level.block.Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getClickedFace().getOpposite();
+        return this.defaultBlockState().setValue(FACING, direction);
     }
 }
