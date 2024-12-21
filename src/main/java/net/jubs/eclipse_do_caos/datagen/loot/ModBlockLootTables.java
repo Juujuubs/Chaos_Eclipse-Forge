b6c154a7ecbc19f6e443ae7f665cd2e9144f86fc
Ju <gabrielgcm.martins@gmail.com>
2024-12-21 11:33:43 -0300

    Added the Functioning Eden Leaves Drops

--- src/main/java/net/jubs/eclipse_do_caos/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/jubs/eclipse_do_caos/datagen/loot/ModBlockLootTables.java
@@ -2,21 +2,29 @@ package net.jubs.eclipse_do_caos.datagen.loot;
 
 import net.jubs.eclipse_do_caos.block.ModBlocks;
 import net.jubs.eclipse_do_caos.item.ModItems;
+import net.minecraft.advancements.critereon.EnchantmentPredicate;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.ApplyExplosionDecay;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
+
+import static net.jubs.eclipse_do_caos.item.ModItems.EDEN_APPLE;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
     public ModBlockLootTables() {
@@ -56,13 +64,8 @@ public class ModBlockLootTables extends BlockLootSubProvider {
                 block -> createCopperLikeOreDrops(ModBlocks.ESSENCE_ORE_BLOCK.get(), ModItems.ESSENCE.get()));
 
         this.add(ModBlocks.EDEN_LEAVES.get(), block ->
-                createLeavesDrops(block, ModBlocks.EDEN_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
-        this.add(ModBlocks.EDEN_LEAVES.get(), item ->
-                createLeavesDrops(item, Block.byItem(ModItems.EDEN_APPLE.get()), 0.036f));
-        this.add(ModBlocks.EDEN_LEAVES.get(), block ->
-                createLeavesDrops(block, Block.byItem(ModItems.EDEN_TREE_APPLE.get()), 0.02f));
-        this.add(ModBlocks.EDEN_LEAVES.get(), block ->
-                createLeavesDrops(block, Block.byItem(Items.STICK), 0.12f));
+                createEdenLeavesDrops(block, ModBlocks.EDEN_SAPLING.get(), EDEN_APPLE.get(), ModItems.EDEN_TREE_APPLE.get(), Items.STICK));
+
 
         this.add(ModBlocks.EDEN_SIGN.get(), block ->
                 createSingleItemTable(ModItems.EDEN_SIGN.get()));
@@ -86,5 +89,41 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
     }
 
+    protected LootTable.Builder createEdenLeavesDrops(Block leavesBlock, Block sapling, Item apple, Item treeApple, Item stick) {
+        LootPool.Builder silkTouchPool = LootPool.lootPool()
+                .when(MatchTool.toolMatches(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.Ints.atLeast(1)))))
+                .add(LootItem.lootTableItem(leavesBlock))
+                .apply(ApplyExplosionDecay.explosionDecay());
+
+        LootPool.Builder shearsPool = LootPool.lootPool()
+                .when(MatchTool.toolMatches(ItemPredicate.Builder.item().of(Items.SHEARS)))
+                .add(LootItem.lootTableItem(leavesBlock))
+                .apply(ApplyExplosionDecay.explosionDecay());
+
+        LootPool.Builder saplingPool = LootPool.lootPool()
+                .add(LootItem.lootTableItem(sapling))
+                .when(LootItemRandomChanceCondition.randomChance(0.12f));
+
+        LootPool.Builder applePool = LootPool.lootPool()
+                .add(LootItem.lootTableItem(apple))
+                .when(LootItemRandomChanceCondition.randomChance(0.027f));
+
+        LootPool.Builder treeApplePool = LootPool.lootPool()
+                .add(LootItem.lootTableItem(treeApple))
+                .when(LootItemRandomChanceCondition.randomChance(0.015f));
+
+        LootPool.Builder stickPool = LootPool.lootPool()
+                .add(LootItem.lootTableItem(stick))
+                .when(LootItemRandomChanceCondition.randomChance(0.09f));
+
+        return LootTable.lootTable()
+                .withPool(silkTouchPool)
+                .withPool(shearsPool)
+                .withPool(saplingPool)
+                .withPool(applePool)
+                .withPool(treeApplePool)
+                .withPool(stickPool);
     }
+
+}
 
